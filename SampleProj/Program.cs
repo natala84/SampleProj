e96dd4287b833183d9ac5a42f844c8fb7d038a5d
Arjun Natal <ArjunN@magenic.com>
2020-06-25 11:43:18 +0800

    Add project files.

---
+++ SampleProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
